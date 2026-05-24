d4f94947a79cf57251655b8517159ac16d1d131e
Anarasty <murloc.ninja16@gmail.com>
2026-05-24 20:11:43 +0300

    added page header

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,21 @@ import { ThemeProvider } from '@/components/ThemeProvider';
 import { SidebarInset, SidebarProvider } from '@/components/ui/sidebar';
 import { AppSidebar } from '@/components/AppSidebar';
 import { Header } from '@/components/Header';
+import { Page, PageHeader } from '@/components/Page';
 
 function App() {
   return (
     <ThemeProvider>
-      <SidebarProvider >
+      <SidebarProvider>
         <AppSidebar />
         <SidebarInset>
           <Header />
+
+          <main>
+            <Page>
+              <PageHeader/>
+            </Page>
+          </main>
         </SidebarInset>
       </SidebarProvider>
     </ThemeProvider>
